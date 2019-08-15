3988bd5e15914d1c4a04ffb39b0df8e5239692e9
yaozhaoyang <523926245@qq.com>
2019-08-15 10:36:51 +0800

    新增Popup组件

---
+++ src/components/popup/popup.vue
@@ -0,0 +1,41 @@
+<template>
+    <div v-show="isShow" class="y-popup" :class="[position ? 'y-popup--'+position : '']">
+        <slot>
+            <div></div>
+        </slot>
+    </div>
+</template>
+
+<script>
+import Mask from "@/components/mask";
+export default {
+    name:"yPopup",
+    props:{
+        position:{
+            type:String,
+            default:'center'
+        },
+        value:{
+            type:Boolean,
+            default:false
+        }
+    },
+    computed:{
+        isShow:{
+            get(){
+                return this.value
+            },
+            set(){
+                
+            }
+        }
+    },
+    mounted(){
+        Mask.show(this,{maskClose:true})
+    }
+}
+</script>
+
+<style>
+
+</style>
