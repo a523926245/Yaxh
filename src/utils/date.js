925a00b8aaeaa1b3e1897a16759b400eed552e8f
yaozhaoyang <523926245@qq.com>
2019-08-23 17:18:53 +0800

    add:add datePicker component

---
+++ src/utils/date.js
@@ -0,0 +1,21 @@
+export const isDate = (date) => {
+    return (
+        Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date.getTime())
+    );
+}
+export const isNaN = (value) =>{
+    if (Number.isNaN) {
+      return Number.isNaN(value);
+    }
+    // eslint-disable-next-line no-self-compare
+    return value !== value;
+}
+export const formatDate = (date) =>{
+    let year,month,day,dateArr;
+    let d = new Date(date);
+    year = d.getFullYear();
+    month = (d.getMonth() + 1) > 10 ? `${d.getMonth() + 1}` : `0${d.getMonth() + 1}`;
+    day = d.getDate() > 10 ? `${d.getDate()}` : `0${d.getDate()}`;
+    dateArr = [year,month,day]
+    return dateArr
+}
